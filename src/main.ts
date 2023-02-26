e2ce0e11c05b3eecfade3497a178229e38bb10b6
zeekcheung <zeekcheung@gmail.com>
2023-02-26 17:50:25 +0800

    feat: 配置 HTTPS 服务

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { randomUUID } from 'crypto';
 import * as session from 'express-session';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 import { createClient } from 'redis';
 import { ORIGIN, SESSION_ID_NAME } from './common/constant';
 import { allowPrivateNetworkMiddleware } from './middleware/allow-private-network.middle';
@@ -13,8 +15,15 @@ import { AppModule } from './modules/app/app.module';
 const RedisStore = require('connect-redis')(session);
 
 async function bootstrap() {
+  // 创建 https 服务器
+  const httpsOptions = {
+    key: readFileSync(resolve('./secrets/private-key.pem')),
+    cert: readFileSync(resolve('./secrets/public-certificate.pem')),
+  };
+
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'debug'],
+    httpsOptions,
   });
 
   // 获取环境变量
